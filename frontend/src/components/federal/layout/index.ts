ae47a0d84f608e84e41619727bb4cbd2671ea1e1
BenedictGPT <130986576+BenedictGPT@users.noreply.github.com>
2025-11-03 00:18:51 -0500

    feat: Add Federal layout components (Header, Sidebar, Footer, DashboardLayout) - complete navigation system

---
+++ frontend/src/components/federal/layout/index.ts
@@ -0,0 +1,14 @@
+// Federal Government Layout Components
+
+export { Header } from './Header';
+export type { HeaderProps } from './Header';
+
+export { Sidebar } from './Sidebar';
+export type { SidebarProps, SidebarItem } from './Sidebar';
+
+export { Footer } from './Footer';
+export type { FooterProps } from './Footer';
+
+export { DashboardLayout } from './DashboardLayout';
+export type { DashboardLayoutProps } from './DashboardLayout';
+
